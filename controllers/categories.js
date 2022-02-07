7cff9d7f004d1cded6841dbf90240e3c7947634c
EloiGry <eloi.grychta@gmail.com>
2022-02-07 12:19:40 +0100

    filter category

--- controllers/categories.js
+++ controllers/categories.js
@@ -16,11 +16,12 @@ exports.createCategory = async (req, res) => {
 }
 
 exports.getCategory = async (req, res) => {
+    
     try {
         const category = await Category.find()
         .populate({
             path: 'products',
-            select: 'name image description kiloPrice'
+            select: 'name image description kiloPrice',
         })
         .exec()
         res.json(category)
@@ -30,12 +31,23 @@ exports.getCategory = async (req, res) => {
 }
 
 exports.getOneCategory =  async (req, res) => {
+    const {sort, filter} = req.query
+    let findParams = {}
+
+if (filter) {
+    findParams = {
+        ...findParams,
+        name : {$regex : filter, $options: "i"}
+    }
+}
     const {id} = req.params
     try {
         const oneCategory = await Category.findById(id)
         .populate({
             path: 'products',
-            select: 'name image kiloPrice pricePerPiece'
+            select: 'name image kiloPrice pricePerPiece',
+            match : findParams,
+            options: { sort: { 'kiloPrice': sort }}
         })
         .exec()
         res.json(oneCategory)
